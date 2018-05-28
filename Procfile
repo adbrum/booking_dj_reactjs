cf3d7bacdfd10d76d1898b1c214532e9b6ccb2f4
aleal <adriano.leal@exlcusivkey.pt>
2018-05-28 16:00:55 +0100

    --

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn booking_dj_reactjs.wsgi --log-file -
+web: gunicorn booking_dj_react.wsgi --log-file -
--- booking_dj_react/wsgi.py
+++ booking_dj_react/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booking_dj_reactjs.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booking_dj_react.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
