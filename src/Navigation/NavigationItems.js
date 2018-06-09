a95276f60eec3b20faaba6767dd8e953cbc356cb
aleal <adriano.leal@exlcusivkey.pt>
2018-06-09 15:54:54 +0100

    Add modulo @material-ui in project

--- src/Navigation/NavigationItems.js
+++ src/Navigation/NavigationItems.js
@@ -1,19 +1,19 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import AppBar from '../@material-ui/core/AppBar';
-import Toolbar from '../@material-ui/core/Toolbar';
-import Typography from '../@material-ui/core/Typography';
-import IconButton from '../@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '../@material-ui/core/MenuItem';
-import Menu from '../@material-ui/core/Menu';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import MenuItem from 'material-ui/Menu/MenuItem';
+import Menu from 'material-ui/Menu';
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
-import Button from "../@material-ui/core/es/Button/Button";
+import Button from "material-ui/Button";
 import {loadLogout} from "../actions";
-import withStyles from '../@material-ui/core/styles/withStyles';
+import {withStyles} from 'material-ui/styles';
 const styles = {
     root: {
         flexGrow: 1,
